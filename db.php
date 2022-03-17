6cab3249d8e9159b4026d87bc5aec46730940bf6
mohammed zubair <moh.775123554@gmail.com>
2022-03-17 13:36:58 +0300

    add cart product

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+// conection to database
+$con = mysqli_connect("localhost","root","","cart");
+	if (mysqli_connect_errno()){
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		die();
+		}
+?>
